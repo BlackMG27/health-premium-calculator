f3c5e34e512091fe45e44fb30b133beeed91c29f
BlackMG27 <oike279@gmail.com>
2024-10-21 07:44:29 -0500

    added comments and started form

---
+++ assets/calculator.js
@@ -0,0 +1,11 @@
+//grab all of the values from the form 
+//create an addEventListener function on submit 
+//grab values from Kate's chart 
+//create select value options 
+//work the math 
+  //multiply the hourly rate by 3%
+  //multiply new rate by bi-weekly hours 
+  //grab the health plan value 
+  //multiply it by 10% and add it to health plan 
+  //subtract the new health plan from the new wage 
+  //display the answer 
